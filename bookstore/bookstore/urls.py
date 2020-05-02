eb70295785237b18593e34f2e95f1c03f3ddb1d9
sanix sandel <sanicksikani@gmail.com>
2020-05-02 22:17:57 +0300

    end chapter 5

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -18,5 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #user management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    #local apps
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
