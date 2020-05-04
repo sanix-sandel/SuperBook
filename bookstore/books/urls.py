67ef52c670aa4ffaf42f1cf3b1f2e5cb7a70e7b3
sanix sandel <sanicksikani@gmail.com>
2020-05-04 21:04:05 +0300

    end chapter 15

--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView
+from .views import (
+    BookListView,
+    BookDetailView,
+    SearchResultListView
+)
 
 urlpatterns=[
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultListView.as_view(), name='search_results'),
 ]
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -18,3 +18,14 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name='books/book_detail.html'
     login_url='account_login'
     permission_required='books.special_status'
+
+class SearchResultListView(ListView):
+    model=Book
+    context_object_name='book_list'
+    template_name='books/search_results.html'
+
+    def get_query_set(self):
+        query=self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__contains=query) | Q(title__icontains=query)
+        )
